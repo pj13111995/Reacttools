56a69b94328755a064768598501b9ca0fc3be04c
pj13111995 <praveen.poonja@novigosolutions.com>
2018-12-14 18:25:26 +0530

    Updates

---
+++ precache-manifest.9b194d367494d23e457f2a24e85ebb46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/Reacttools/static/media/click2.fe0ded65.wav"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/Reacttools/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "e2d6a6e7288b5e83c8c6",
+    "url": "/Reacttools/static/js/runtime~main.e2d6a6e7.js"
+  },
+  {
+    "revision": "98d08c64ebf4155d3f1e",
+    "url": "/Reacttools/static/js/main.98d08c64.chunk.js"
+  },
+  {
+    "revision": "81b776bcd7c75a42d943",
+    "url": "/Reacttools/static/js/1.81b776bc.chunk.js"
+  },
+  {
+    "revision": "98d08c64ebf4155d3f1e",
+    "url": "/Reacttools/static/css/main.0723bd65.chunk.css"
+  },
+  {
+    "revision": "9227425ac3ca9b6c6478708cccd3a9f5",
+    "url": "/Reacttools/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Reacttools/precache-manifest.9b194d367494d23e457f2a24e85ebb46.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Reacttools/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
